b809b64794922940800a41f683b9c2515e3fa219
Andrew Monteith <andrew.monteith1@gmail.com>
2015-05-05 01:45:49 +1000

    fixed sql

--- hiddenmonkey/hiddenmonkey/spiders/hidden_monkey.py
+++ hiddenmonkey/hiddenmonkey/spiders/hidden_monkey.py
@@ -7,23 +7,35 @@ import MySQLdb
 class HiddenMonkeySpider(scrapy.Spider):
     db = MySQLdb.connect(host="localhost", user="root", passwd="adaymaycomewhen", db="hidden_monkey")
     db_cur = db.cursor()
-    db_cur.execute('SELECT video_url FROM video_list;')
+    db_cur.execute('SELECT video_url,video_id FROM video_list;')
     start_urls = []
-    for row in db_cur.fetchall() :
+    video_list = db_cur.fetchall()
+    for row in video_list:
         start_urls.append(row[0])
-
+    db.close()
     name = "YouTube"
     allowed_domains = ["www.youtube.com"]
 
 
     def parse(self, response):
-        views = response.css('div .watch-view-count::text').extract()
-        log_file = open('log', 'a')
+        insert_sql = "INSERT INTO video_stats(video_id , video_views , time_collected) "
         db = MySQLdb.connect(host="localhost", user="root", passwd="adaymaycomewhen", db="hidden_monkey")
-        cur = db.cursor()
-        cur.execute('INSERT INTO video_stats(video_id,video_url) VALUES;')
-        viewsString = str(views)
-        thisTime = str(time.time())
+        db_cur = db.cursor()
 
-        log_file.write(viewsString + thisTime)
-        log_file.close()
+        views = response.css('div .watch-view-count::text').extract()
+        view_string = str(views)
+        view_count = ""
+        for i in re.findall(r'\b\d+\b', view_string):
+            print(i)
+            view_count = view_count + str(i)
+        print("*****---->>> views ="+view_count)
+        this_time = str(time.time())
+
+        sub_query_for_id = "(SELECT video_id FROM video_list WHERE video_url='"+response.url+"')"
+        print(insert_sql+"VALUE( "+sub_query_for_id+" , " +view_count + " , '" + this_time + "')")
+        db_cur.execute(insert_sql+"VALUE( "+sub_query_for_id+" , " +view_count + " , '" + this_time + "')")
+        print("******----->>>"+view_count+"\n")
+        print("******----->>>"+this_time)
+        db.commit()
+        db.close()
+
