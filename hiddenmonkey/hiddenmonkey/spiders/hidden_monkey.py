ab818b2e436d2d3a4ee8a4da3b92948cf6e88402
Andrew Monteith <andrew.monteith1@gmail.com>
2015-05-06 22:50:49 +1000

    update stuff

--- hiddenmonkey/hiddenmonkey/spiders/hidden_monkey.py
+++ hiddenmonkey/hiddenmonkey/spiders/hidden_monkey.py
@@ -21,21 +21,13 @@ class HiddenMonkeySpider(scrapy.Spider):
         insert_sql = "INSERT INTO video_stats(video_id , video_views , time_collected) "
         db = MySQLdb.connect(host="localhost", user="root", passwd="adaymaycomewhen", db="hidden_monkey")
         db_cur = db.cursor()
-
         views = response.css('div .watch-view-count::text').extract()
         view_string = str(views)
         view_count = ""
         for i in re.findall(r'\b\d+\b', view_string):
-            print(i)
-            view_count = view_count + str(i)
-        print("*****---->>> views ="+view_count)
-        this_time = str(time.time())
-
+            view_count +=str(i)
         sub_query_for_id = "(SELECT video_id FROM video_list WHERE video_url='"+response.url+"')"
-        print(insert_sql+"VALUE( "+sub_query_for_id+" , " +view_count + " , '" + this_time + "')")
-        db_cur.execute(insert_sql+"VALUE( "+sub_query_for_id+" , " +view_count + " , '" + this_time + "')")
-        print("******----->>>"+view_count+"\n")
-        print("******----->>>"+this_time)
+        db_cur.execute(insert_sql+"VALUE( "+sub_query_for_id+" , " +view_count + " , '" + str(time.time()) + "')")
         db.commit()
         db.close()
 
