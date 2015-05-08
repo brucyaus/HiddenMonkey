198de27fd4cf5610e004913035bcd8cf56a28e79
Andrew Monteith <andrew.monteith1@gmail.com>
2015-05-08 13:05:33 +1000

    bug fix

--- sage_monkey/sage_monkey.py
+++ sage_monkey/sage_monkey.py
@@ -12,7 +12,7 @@ rows = cursor.fetchall()
 
 data_frame = pd.DataFrame([[ij for ij in i] for i in rows])
 data_frame.rename(columns={0:"video_id",1:"video_views",2:"time_collected"})
-video_ids = data_frame["video_id"]
+video_ids = data_frame['video_id']
 trace1 = Scatter(x=data_frame["time_collected"],y=data_frame["video_views"],text=data_frame,mode="markers")
 layout = Layout(title="YouTube Views over time", xaxis=XAxis(title="Time"),yaxis=YAxis(title="Views"))
 data = Data([trace1])
